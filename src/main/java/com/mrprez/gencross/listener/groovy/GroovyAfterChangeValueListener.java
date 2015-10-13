132e57e77a85ecb014160b4de903944903cfc217
mrprez <gagneret@yahoo.fr>
2015-10-13 15:46:33 +0200

    Get work

---
+++ src/main/java/com/mrprez/gencross/listener/groovy/GroovyAfterChangeValueListener.java
@@ -0,0 +1,49 @@
+package com.mrprez.gencross.listener.groovy;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
+
+import com.mrprez.gencross.Property;
+import com.mrprez.gencross.listener.AfterChangeValueListener;
+import com.mrprez.gencross.value.Value;
+
+public class GroovyAfterChangeValueListener extends AfterChangeValueListener {
+	
+	private String script;
+
+	@Override
+	public void callAfterChangeValue(Property property, Value oldValue) throws Exception {
+		Binding binding = new Binding();
+		binding.setProperty("property", property);
+		binding.setProperty("oldValue", oldValue);
+		binding.setProperty("personnage", property.getPersonnage());
+		
+		ImportCustomizer importCustomizer = new ImportCustomizer();
+		importCustomizer.addImports("com.mrprez.gencross.value.*");
+		CompilerConfiguration compilerConfiguration = new CompilerConfiguration();
+		compilerConfiguration.addCompilationCustomizers(importCustomizer);
+		
+		GroovyShell groovyShell = new GroovyShell(binding, compilerConfiguration);
+		
+		groovyShell.evaluate(script);
+	}
+
+	@Override
+	public Map<String, String> getArgs() {
+		Map<String, String> args = new HashMap<String, String>(1);
+		args.put("script", script);
+		return null;
+	}
+
+	@Override
+	public void setArgs(Map<String, String> args) throws Exception {
+		script = args.get("script");
+	}
+
+}
